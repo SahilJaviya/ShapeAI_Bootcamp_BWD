b7d6081cce2a6cec5fa5446ef9a67c49b313b330
SahilJaviya <85433767+SahilJaviya@users.noreply.github.com>
2021-06-07 13:20:10 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import "./styles.css";
+import react from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Note />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import react from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and react js</h1>
+      <p>
+        This was an amazing bootcamp taken up by shaurya sinha.We covered
+        everything from scratch including Javascript, react js and HTML
+      </p>
+    </div>
+  );
+}
+
+export default Note;
